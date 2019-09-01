7bb41a60dcc65633ebb5c8799e96dff1e323acb8
yernar-zh <yernar.zharkynuly@gmail.com>
2019-09-01 14:43:34 +0600

    wx

--- global-ejb/src/main/java/kz/kazmoto/glob/exceptions/ErrorCodeException.java
+++ global-ejb/src/main/java/kz/kazmoto/glob/exceptions/ErrorCodeException.java
@@ -1,13 +1,15 @@
 package kz.kazmoto.glob.exceptions;
 
+import javax.ejb.ApplicationException;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 
-public class ErrorCodeException extends WebApplicationException {
+@ApplicationException(rollback = true)
+public class ErrorCodeException extends RuntimeException {
     private int code;
     private Response.Status status;
 
-    protected ErrorCodeException(String message, Response.Status status, int code) {
+    ErrorCodeException(String message, Response.Status status, int code) {
         super(message);
         this.status = status;
         this.code = code;
@@ -19,9 +21,5 @@ public class ErrorCodeException extends WebApplicationException {
 
     public Response.Status getStatus() {
         return status;
-    }
-
-    public void setStatus(Response.Status status) {
-        this.status = status;
     }
 }
